6a276842c7c8628ae7ef37dc38d1bb46269d2d97
Aeromazing08 <55785090+Aeromazing08@users.noreply.github.com>
2019-10-02 10:48:24 +0800

    Add files via upload

---
+++ Practice2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Practice2
+{
+	public static void main(String[]args)	
+	{
+		Scanner sc = new Scanner(System.in);
+		String x1, y1;
+		Double x, y, sum;
+
+
+		System.out.println("Very Good ");
+		System.out.println("Input number :");
+		x = sc.nextDouble();
+
+		System.out.println("Input number :");
+		y = sc.nextDouble();
+		sum = x+ y;
+
+		System.out.println("Results is : " +sum);
+
+		
+	}
+}
